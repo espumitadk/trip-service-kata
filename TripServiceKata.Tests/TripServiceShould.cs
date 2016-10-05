678727e4bd2b61802656f21634ed69ffdaecb364
David Falcón Naranjo <david_el_125@hotmail.com>
2016-10-05 09:32:33 +0100

    green: throw_and_exception_when_the_user_is_not_logged

---
+++ TripServiceKata.Tests/TripServiceShould.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using NSubstitute;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
+
+namespace TripServiceKata.Tests
+{
+    [TestFixture]
+    public class TripServiceShould
+    {
+        [Test]
+        public void throw_and_exception_when_the_user_is_not_logged()
+        {
+            var tripService = Substitute.For<TripService>();
+            tripService.GetLoggedUser().Returns((User.User) null);
+
+            Action action = () => tripService.GetTripsByUser(null);
+
+            action.ShouldThrow<UserNotLoggedInException>();
+
+
+
+        }
+    }
+}
